e82720b39b530e1e0e8fed7c673cc0d977b6b8e0
NikolayLV <Nikolayd2002@gmail.com>
2023-07-19 18:20:38 +0300

    Wrote a future plans for this project. Now you can choose your keyboard layout. Some changes in JS.

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ foodImg.src = "img/food.png";
 let box = 32;
 
 let bestScoreP = document.querySelector(".bestScore")
+let select = document.querySelector("select");
 let bestScore = 0;
 
 let game;
@@ -27,25 +28,62 @@ snake[0] = {
     y: 10 * box,
 };
 
-document.addEventListener("keydown", direction);
+let kb1 = document.querySelector("#kb1");
+let kb2 = document.querySelector("#kb2");
+let langHash = select.value;
+
+function chooseKeyBoard() {
+
+    if (kb1.checked) {
+        document.addEventListener("keydown", direction);
+        console.log(kb1.checked)
+    } else if (kb2.checked) {
+        document.addEventListener("keydown", directionSecond);
+        console.log(kb2.checked)
+    } else {
+        if (langHash == "ru") {
+            alert(langArr['keyAlert']['ru'])
+        } else if (langHash == "en") {
+            alert(langArr['keyAlert']['en'])
+        } else if (langHash == "ua") {
+            alert(langArr['keyAlert']['ua'])
+        } else {
+            alert(langArr['keyAlert']['en'])
+        }
+    }
+
+}
+
 
 let dir;
 
 function direction(event) {
-    if(event.keyCode == 37 && dir != "right")
+    if (event.keyCode == 37 && dir != "right")
         dir = "left";
-    else if(event.keyCode == 38 && dir != "down")
+    else if (event.keyCode == 38 && dir != "down")
         dir = "up";
-    else if(event.keyCode == 39 && dir != "left")
+    else if (event.keyCode == 39 && dir != "left")
         dir = "right";
-    else if(event.keyCode == 40 && dir != "up")
+    else if (event.keyCode == 40 && dir != "up")
         dir = "down";
 
 }
 
-function eatTail (head, arr) {
-    for(let i = 0; i < arr.length; i++) {
-        if(head.x == arr[i].x && head.y == arr[i].y)
+function directionSecond(event) {
+    if (event.keyCode == 65 && dir != "right")
+        dir = "left";
+    else if (event.keyCode == 87 && dir != "down")
+        dir = "up";
+    else if (event.keyCode == 68 && dir != "left")
+        dir = "right";
+    else if (event.keyCode == 83 && dir != "up")
+        dir = "down";
+
+}
+
+function eatTail(head, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (head.x == arr[i].x && head.y == arr[i].y)
             clearInterval(game);
     }
 }
@@ -55,8 +93,8 @@ function drawGame() {
 
     ctx.drawImage(foodImg, food.x, food.y);
 
-    for(let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = i == 0 ? "green" : "purple";
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i == 0 ? "black" : "#B18597FF";
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
@@ -73,25 +111,25 @@ function drawGame() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(snakeX == food.x && snakeY == food.y) {
+    if (snakeX == food.x && snakeY == food.y) {
         score++;
-     food = {
-    x: Math.floor((Math.random() * 17 + 1)) * box,
-    y: Math.floor((Math.random() * 15 + 3)) * box,
-};
+        food = {
+            x: Math.floor((Math.random() * 17 + 1)) * box,
+            y: Math.floor((Math.random() * 15 + 3)) * box,
+        };
 
     } else {
         snake.pop();
     }
 
-    if(snakeX < box || snakeX > box * 17
-       || snakeY < 3 * box || snakeY > box * 17)
+    if (snakeX < box || snakeX > box * 17
+        || snakeY < 3 * box || snakeY > box * 17)
         clearInterval(game);
 
-    if(dir == "left") snakeX -= box;
-    if(dir == "right") snakeX +=box;
-    if(dir == "up") snakeY -=box;
-    if(dir == "down") snakeY +=box;
+    if (dir == "left") snakeX -= box;
+    if (dir == "right") snakeX += box;
+    if (dir == "up") snakeY -= box;
+    if (dir == "down") snakeY += box;
 
     let newHead = {
         x: snakeX,
@@ -122,15 +160,20 @@ function restartGame() {
 
     if (lvl1.checked) {
         game = setInterval(drawGame, 150);
-    }
-    else if (lvl2.checked) {
+    } else if (lvl2.checked) {
         game = setInterval(drawGame, 100);
-    }
-    else if (lvl3.checked) {
+    } else if (lvl3.checked) {
         game = setInterval(drawGame, 50);
-    }
-    else {
-        alert("Выберите уровень сложности!")
+    } else {
+        if (langHash == "ru") {
+            alert(langArr['alert']['ru'])
+        } else if (langHash == "en") {
+            alert(langArr['alert']['en'])
+        } else if (langHash == "ua") {
+            alert(langArr['alert']['ua'])
+        } else {
+            alert(langArr['alert']['en'])
+        }
     }
 
 
@@ -138,13 +181,9 @@ function restartGame() {
 
 let button = document.querySelector(".learn-more")
 button.addEventListener('click', restartGame);
+button.addEventListener('click', chooseKeyBoard);
 
-// let form = document.querySelector(".form")
-// form.addEventListener("submit", function(event) {
-//
-// })
 
-let select = document.querySelector("select");
 select.addEventListener("change", changeLang);
 const allLang = ["en", "ru", "ua"];
 
@@ -154,32 +193,23 @@ function changeURLLang() {
 
 function changeLang() {
     let hash = select.value;
+
     if (hash == "ru") {
         document.querySelector(".bScore").innerHTML = langArr['score'][hash];
         document.querySelector(".learn-more").innerHTML = langArr['start'][hash];
         document.querySelector(".choose__dif").innerHTML = langArr['difficulty'][hash];
-    }
-    else if (hash == "en") {
+        document.querySelector(".layout").innerHTML = langArr['layout'][hash];
+    } else if (hash == "en") {
         document.querySelector(".bScore").innerHTML = langArr['score'][hash];
         document.querySelector(".learn-more").innerHTML = langArr['start'][hash];
         document.querySelector(".choose__dif").innerHTML = langArr['difficulty'][hash];
-    }
-    else if (hash == "ua") {
+        document.querySelector(".layout").innerHTML = langArr['layout'][hash];
+    } else if (hash == "ua") {
         document.querySelector(".bScore").innerHTML = langArr['score'][hash];
         document.querySelector(".learn-more").innerHTML = langArr['start'][hash];
         document.querySelector(".choose__dif").innerHTML = langArr['difficulty'][hash];
+        document.querySelector(".layout").innerHTML = langArr['layout'][hash];
     }
-    else {
-        document.querySelector(".bScore").innerHTML = langArr['score']['en'];
-    }
-    // hash = hash.substring(1);
-    // console.log(hash);
-    // if (!allLang.includes(hash)) {
-    //     location.href = window.location.pathname + "#en";
-    //     location.reload();
-    // }
-    // select.value = hash;
-    // document.querySelector(".bScore").innerHTML = langArr['score'][hash];
 }
 
 changeLang();
